768a4f2eb27093969897dd4b51bf1dfd8b14084e
GUISSOU756 <amsaguissou87@gmail.com>
2024-12-20 08:24:15 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ document.getElementById('welcomeButton').addEventListener('click', function() {
     document.getElementById('welcomeMessage').textContent = 'Nous sommes ravis de vous 
    accueillir sur le site dédié à l\'EPO. Explorez nos programmes et nos initiatives!';
    });
+function changeDynamicContent() {
+ document.getElementById('dynamicContent').textContent = 'Le contenu a été changé 
+grâce à JavaScript!';
+}
